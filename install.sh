aaa2ef67d9c67476b09ad052698bfacd4e60ecaa
rotemil <51912166+rotemil@users.noreply.github.com>
2019-06-23 09:02:33 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# update installed packages 
+
+sudo yum update -y 
+sudo yum install git -y
+export HOME=/root
+
+npm --version
+
+if [[ $? != 0 ]]; then << EOF
+
+          git clone https://github.com/mrvautin/adminMongo.git && cd adminMongo
+
+            curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+            . ~/.nvm/nvm.sh
+
+           nvm install node
+        
+         npm install
+        
+        cd.
+EOF
+
+fi
+
+# changing localhost to everyone.
+sudo sed -i 's/localhost/0.0.0.0/g' /home/ec2-user/adminmongo/app.js
